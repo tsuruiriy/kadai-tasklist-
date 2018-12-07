0d83cbf753162c1eeb970c3512c10b478fc73d93
eri.ikeda <moonlighttango2001@yahoo.co.jp>
2018-12-07 07:03:59 +0000

    削除ボタンの修正

--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -21,7 +21,9 @@
     </table>
     
     <div><a class="btn btn-primary  col-md-3 col-sm-10" href="{{ action('TasksController@edit', $task->id) }}">編集する</a></div>
-    <div><a class="btn btn-danger mt-3  col-md-3 col-sm-10" href="{{ action('TasksController@destroy', $task->id) }}">削除する</a></div>
+    {!! Form::model($task, ['route' =>['tasklists.destroy', $task->id], 'method' => 'delete']) !!}
+    {!! Form::submit('削除', ['class' => 'btn btn-danger mt-3 col-md-3 col-sm-10']) !!}
+    {!! Form::close() !!}
 
 
 @endsection
